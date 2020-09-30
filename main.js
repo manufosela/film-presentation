8d2aa439fbf45a93219bcf5431eb63d20d6d4117
Manuel Juan Fosela Águila <manufosela@gmail.com>
2020-09-30 06:26:23 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const texto = [
+  'KAIROS DS',
+  'presents',
+  'Lead The Change',
+  'the film',
+  'Directed by',
+  'Lucía Pablos',
+  'Produced by',
+  'Carlos Moure',
+  'Interpreted by',
+  'Carlos Matias in',
+  'AgileLeadMan',
+  'Carlos Cañado in',
+  'InnovationMan',
+  ''
+]
